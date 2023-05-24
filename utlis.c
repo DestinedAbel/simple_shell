f6a496cfa8edb82ace6c5c93541a24a809984134
siyasanga98 <siyasangavilakati98@gmail.com>
2023-05-24 16:09:18 -0700

    simple shell group work

--- utlis.c
+++ utlis.c
@@ -1,4 +1,5 @@
-#include "shell.h"
+#include <unistd.h> 
+#include <stdlib.h>
 #include <string.h>
 
 /**
@@ -6,96 +7,90 @@
  */
 void print_prompt(void)
 {
-	if (isatty(STDIN_FILENO) == 1)
-		write(STDOUT_FILENO, "$ ", 2);
+    if (isatty(STDIN_FILENO) == 1)
+        write(STDOUT_FILENO, "$ ", 2);
 }
 
 /**
  * free_args - free an array of string
  * @av: an array of strings
  */
-
 void free_args(char **av)
 {
-	int i = 0;
+    char **ptr = av;
 
-	while (av[i] != NULL)
-	{
-		free(av[i]);
-		i++;
-	}
-	free(av);
+    while (*ptr)
+    {
+        free(*ptr);
+        ptr++;
+    }
+    free(av);
 }
 
 /**
  * _getenv - gets an environment variable
  * @name: the environment variable name
- * @e: an array of environment variables
  * Return: the environment variable value
  */
-
-char *_getenv(const char *name, char **e)
+char *_getenv(const char *name)
 {
-	char *variable, *token, *path;
-	char **envp = e;
+    extern char **environ;
+    char *variable, *token, *path;
+    char **envp = environ;
 
-	while (*envp)
-	{
-		variable = malloc(sizeof(char) * (_strlen(*envp) + 1));
-		if (variable == NULL)
-			return (NULL);
-		_strcpy(variable, *envp);
-		token = strtok(variable, "=");
-		if (token != NULL && _strcmp(token, name) == 0)
-		{
-			token = strtok(NULL, "\0");
-			path = malloc(sizeof(char) * (_strlen(token) + 1));
-			if (path == NULL)
-				return (NULL);
-			_strcpy(path, token);
-			free(variable);
-			return (path);
-		}
-		envp++;
-		free(variable);
-	}
-	return (NULL);
+    while (*envp)
+    {
+        variable = malloc(strlen(*envp) + 1);
+        if (variable == NULL)
+            return NULL;
+        strcpy(variable, *envp);
+        token = strtok(variable, "=");
+        if (token != NULL && strcmp(token, name) == 0)
+        {
+            token = strtok(NULL, "\0");
+            path = malloc(strlen(token) + 1);
+            if (path == NULL)
+                return NULL;
+            strcpy(path, token);
+            free(variable);
+            return path;
+        }
+        envp++;
+        free(variable);
+    }
+    return NULL;
 }
 
 /**
  * execute_exit - launch exit command
  * @status: the exit status
  * @av: an array of arguments
- * @cmd: an array of commands
  * @line: the line
  */
-
-void execute_exit(int status, char **av, char *line, char **cmd)
+void execute_exit(int status, char **av, char *line)
 {
-	free_args(av);
-	free(line);
-	free_args(cmd);
-	if (status == 200)
-		exit(0);
-	exit(status);
+    free_args(av);
+    free(line);
+    if (status == 200)
+        exit(0);
+    exit(status);
 }
-
 
 /**
  * ignore_comments - ignore the comments
  * @lineptr : the giving string
  */
-
 void ignore_comments(char *lineptr)
 {
-	int i;
+    int i;
 
-	for (i = 0; lineptr[i] != '\0'; i++)
-	{
-		if (lineptr[i] == '#' && (i == 0 || lineptr[i - 1] == ' '))
-		{
-			lineptr[i] = '\0';
-			return;
-		}
-	}
+    for (i = 0; lineptr[i] != '\0'; i++)
+    {
+        if (lineptr[i] == '#' && (i == 0 || lineptr[i - 1] == ' '))
+        {
+            lineptr[i] = '\0';
+            return;
+        }
+    }
 }
+
